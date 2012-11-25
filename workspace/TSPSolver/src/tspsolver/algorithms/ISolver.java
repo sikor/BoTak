e69f6cc88da20398333de5557a3dc851ac4eb2b7
dominik2cucu <dominik2cucu@gmail.com>
2012-11-25 19:37:25 +0100

    Changed interface of "solve" method. Added GeneticSolver, but hardcoded for now.

--- workspace/TSPSolver/src/tspsolver/algorithms/ISolver.java
+++ workspace/TSPSolver/src/tspsolver/algorithms/ISolver.java
@@ -1,16 +1,13 @@
 package tspsolver.algorithms;
 
-import java.util.Properties;
-
 public interface ISolver {
 	
 	/**
 	 * 
-	 * @param distances
-	 * @return
+	 * Returns instance of class Return.
 	 * 
-	 * take distances between points return the array of point indexes in optimized order
 	 */
-	public int[] solve(double[][] distances, Properties properties);
+	
+	public Return solve();
 
 }
--- workspace/TSPSolver/src/tspsolver/algorithms/genetic/GeneticSolver.java
+++ workspace/TSPSolver/src/tspsolver/algorithms/genetic/GeneticSolver.java
@@ -1,19 +1,191 @@
 package tspsolver.algorithms.genetic;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
+import java.util.Random;
 
 import tspsolver.algorithms.ISolver;
+import tspsolver.algorithms.Return;
 
 public class GeneticSolver implements ISolver{
+	double odleglosci[][] = {{0, 4, 2, 4, 4},
+			{4, 0, 4, 6, 2},
+			{2, 4, 0, 2, 2},
+			{4, 6, 2, 0, 4},
+			{4, 2, 2, 4, 0}};
+	int N = 5, K = 2, M = 1, m = 1;
 
-	@Override
-	public int[] solve(double[][] distances, Properties properties) {
-		// TODO Auto-generated method stub
-		return null;
+	public GeneticSolver(double[][] distances, Properties properties) {
+		//Hardcoded for now and... in polish, booya!
 	}
 
-
+	Random r = new Random();
 	
-	
+	int losuj(int n) {
+		return r.nextInt(n);
+	}
 
+	class Osobnik {
+		Chromosomy dna;
+		double odleglosc;
+	}
+	
+	class Chromosomy {
+		ArrayList<Integer> t;
+		
+		Chromosomy() {
+			t = new ArrayList<Integer>();
+		}
+		
+		Chromosomy(Chromosomy and) {
+			t = (ArrayList<Integer>)and.t.clone();
+		}
+		
+		void wypelnij() {
+			int los;
+			t.add(new Integer(0));
+			for(int i = 1; i < N; ++i) {
+				do {
+					los = losuj(N - 1) + 1;
+				} while(t.contains(new Integer(los)));
+				t.add(new Integer(los));
+			}
+		}
+		
+		void krzyzujZ(Chromosomy and) {
+			int odKtorego = losuj(N - 2) + 1;
+			int ile = losuj(N - odKtorego);
+			
+			List<Integer> pod = and.t.subList(odKtorego, odKtorego + ile);
+			t.removeAll(pod);
+			t.addAll(odKtorego, pod);
+		}
+		
+		void mutuj() {
+			for(int i = 0; i < m; ++i) {
+				zamienLosowe();
+			}
+		}
+		
+		void zamienLosowe() {
+			int ten = losuj(N - 1) + 1, zTym = losuj(N - 1) + 1;
+			Integer i = t.get(ten);
+			t.set(ten, t.get(zTym));
+			t.set(zTym, i);
+		}
+		
+		double odleglosc() {
+			double r = 0;
+			for(int i = 0; i < N -1; ++i) {
+				r += odleglosci[t.get(i)][t.get(i + 1)];
+			}
+			return r;
+		}
+
+		int [] doTablicyIntow() {
+			int r[] = new int[N], i = 0;
+			for(Integer m : t) {
+				r[i] = m;
+				++i;
+			}
+			return r;
+		}
+	}
+	
+	Osobnik obecni[] = null;
+	
+	Osobnik skopiujOsobnika(Osobnik osobnik) {
+		Osobnik kopia = new Osobnik();
+		kopia.odleglosc = osobnik.odleglosc;
+		kopia.dna = new Chromosomy(osobnik.dna);
+		return kopia;
+	}
+	
+	void krzyzuj() {
+		int ten = losuj(N), zTym;
+		do {
+			zTym = losuj(N);
+		} while(zTym == ten);
+		Osobnik wTen = skopiujOsobnika(obecni[ten]),
+				lubTen = skopiujOsobnika(obecni[zTym]);
+		wTen.dna.krzyzujZ(obecni[zTym].dna);
+		wTen.odleglosc = wTen.dna.odleglosc();
+		lubTen.dna.krzyzujZ(obecni[ten].dna);
+		lubTen.odleglosc = lubTen.dna.odleglosc();
+		Osobnik wybrani[] = {obecni[ten], obecni[zTym], wTen, lubTen};
+		double suma = 0;
+		for(Osobnik o : wybrani) {
+			suma += o.odleglosc;
+		}
+		int pierwszy = wybierzSkrzyzowanego(wybrani, suma), drugi;
+		do {
+			drugi = wybierzSkrzyzowanego(wybrani, suma);
+		} while(drugi == pierwszy);
+		obecni[ten] = wybrani[pierwszy];
+		obecni[zTym] = wybrani[drugi];
+	}
+	
+	int wybierzSkrzyzowanego(Osobnik wybrani[], double suma) {
+		int w = 0;
+		double los = losuj((int)suma - 1);
+		while(los > wybrani[w].odleglosc) {
+			los -= wybrani[w].odleglosc;
+			++w;
+		}
+		return w;
+	}
+	
+	void krzyzowanie() {
+		for(int i = 0; i < K; ++i) {
+			krzyzuj();
+		}
+	}
+	
+	void mutacja() {
+		for(int i = 0; i < M; ++i) {
+			int l = losuj(N);
+			obecni[l].dna.mutuj();
+			obecni[l].odleglosc = obecni[l].dna.odleglosc();
+		}
+	}
+	
+	public Return solve() {
+		if(obecni == null) {
+			pierwszaPopulacja();
+		} else {
+			krzyzowanie();
+			mutacja();
+		}
+		Return r = new Return();
+		Osobnik naj = najlepszy();
+		r.length = naj.odleglosc;
+		r.path = naj.dna.doTablicyIntow();
+		return r;
+	}
+	
+	Osobnik najlepszy() {
+		Osobnik naj = obecni[0];
+		for(Osobnik c : obecni) {
+			System.out.println(c.odleglosc);
+			if(c.odleglosc < naj.odleglosc) {
+				naj = c;
+			}
+		}
+		return naj;
+	}
+	
+	void pierwszaPopulacja() {
+		obecni = new Osobnik[N];
+		for(int i = 0; i < N; ++i) {
+			obecni[i] = new Osobnik();
+			wypelnijLosowo(obecni[i]);
+		}
+	}
+	
+	void wypelnijLosowo(Osobnik c) {
+		c.dna = new Chromosomy();
+		c.dna.wypelnij();
+		c.odleglosc = c.dna.odleglosc();
+	}
 }
